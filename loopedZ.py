5589c4d21cb1eddc2452af79c4d5b2849bf57593
Mark Hunnell <hunnellm@wssu.edu>
2026-05-04 08:41:34 -0700

    Add Z_game, Zell_game, and Zplus_game functions
    Implemented Z_game, Zell_game, and Zplus_game functions for graph-based vertex forcing.

--- loopedZ.py
+++ loopedZ.py
@@ -2,12 +2,142 @@ from collections import deque
 
 
 
-# Use the updated version of mr_JG
-load("https://raw.githubusercontent.com/jephianlin/minrank_aux/master/load_all.py")
-load_all(minrank_aux=False)    
+def Z_game(g,B,ban=[]):
+    """
+    Input:
+        g: a simple graph
+        B: a set of initial blue vertices
+        ban: a set of banned vertices
+    Output:
+        return the derived set under the regular CCR-Z.
+        Note: vertices in ban cannot make a force, but are still white neighbors if white.
+    """    
+    V=g.vertices();
+    white_neighbors={}; #a dictionary with the structure {v: list of white neighbors}
+    white_numbers={}; #a dictionary with the structure {v: number of white neighbors}
+    for v in V:
+        nbh=g.neighbors(v);
+        for b in B:
+            try:
+                nbh.remove(b);
+            except ValueError:
+                pass;
+        white_neighbors[v]=nbh;
+        white_numbers[v]=len(nbh);
+    queue=copy(B); #queue stores list of vertices that can possibly make a force
+    derived_set=copy(B); #derived_set stores the set of blue vertices
+    whole_loop=True;
+    while whole_loop: #keep searching if queue!=[]
+        try:
+            v=queue[0];
+            queue.remove(v);
+            if v not in ban and white_numbers[v]==1:
+                u=white_neighbors[v][0]; #the only white neighbor
+                derived_set.append(u); #make the force
+                #update white_numbers, white_neighbors, and queue
+                if white_numbers[u]==1: 
+                    queue.append(u);
+                u_nbr=g.neighbors(u);
+                for w in u_nbr:
+                    white_neighbors[w].remove(u);
+                    white_numbers[w]+=-1;
+                    if w in derived_set and white_numbers[w]==1:
+                        queue.append(w);
+        except IndexError:
+            whole_loop=False;
+    return derived_set;
+
+def Zell_game(g,B,ban=[]): ##every non-isolated vertex can force itself if no other white neighbors.
+    """
+    Input:
+        g: a simple graph
+        B: a set of initial blue vertices
+        ban: a set of banned vertices
+    Output:
+        return the derived set under the CCR-Zell.  
+        That is, in addition to CCR-Z, every non-isolated vertex can force itself if 
+        no other white neighbors.
+        Note: vertices in ban cannot make a force, but are still white neighbors if white.
+    """    
+    #Build h from g by deleting all isolated vertices,
+    # since they are not going to change the outcome. 
+    h=g.copy();
+    for v in g.vertices():
+        if h.degree(v)==0:
+            h.delete_vertex(v);
+    V=h.vertices();
+    white_neighbors={}; #a dictionary with the structure {v: list of white closed neighbors}
+    white_numbers={}; #a dictionary with the structure {v: number of white closed neighbors}
+    for v in V:
+        nbh=h.neighbors(v);
+        nbh.append(v);
+        for b in B:
+            try:
+                nbh.remove(b);
+            except ValueError:
+                pass;
+        white_neighbors[v]=nbh;
+        white_numbers[v]=len(nbh);
+    queue=copy(V); #queue stores list of vertices that can possibly make a force
+    derived_set=copy(B); #derived_set stores the set of blue vertices
+    whole_loop=True;
+    while whole_loop: #keep searching if queue!=[]
+        try:
+            v=queue[0];
+            queue.remove(v);
+            if v not in ban and white_numbers[v]==1:
+                u=white_neighbors[v][0]; #the only white neighbor
+                derived_set.append(u); #make the force
+                #update white_numbers, white_neighbors, and queue
+                u_nbr=h.neighbors(u);
+                u_nbr.append(u);
+                for w in u_nbr:
+                    white_neighbors[w].remove(u);
+                    white_numbers[w]+=-1;
+                    if white_numbers[w]==1:
+                        queue.append(w);
+        except IndexError:
+            whole_loop=False;
+    return derived_set;
+    
+def Zplus_game(g,B):
+    """
+    Input:
+        g: a simple graph
+        B: a set of initial blue vertices
+    Output:
+        return the derived set under the CCR-Zplus.  
+        That is, apply CCR-Z to each white branch.
+        Note: the banned set is not implemented, since Zvcoc has no Zplus version.
+    """   
+    white_graph=g.copy(); #recording the induced subgraph on white
+    derived_set=copy(B); #derived_set stores the set of blue vertices
+    for b in derived_set:
+        white_graph.delete_vertex(b);
+    whole_loop=True;
+    while whole_loop: 
+        whole_loop=False; #open again only when something found.
+        whole_extra_B=[]; #extra blue vertices found in this round
+        partition=white_graph.connected_components();
+        for com in partition:
+            considered_set=copy(com);
+            for v in derived_set:
+                considered_set.append(v);
+            considered_graph=g.subgraph(considered_set); #this is a white branch
+            extra_B=Z_game(considered_graph,derived_set); #apply Z_game to this white branch
+            for v in derived_set:
+                extra_B.remove(v);
+            for v in extra_B:
+                whole_loop=True; #something found, open whole_loop
+                whole_extra_B.append(v);
+        #update new derived_set and new white_graph.
+        for v in whole_extra_B:
+            derived_set.append(v);
+            white_graph.delete_vertex(v);
+    return derived_set;  
     
 
-
+"""
 def pt_plus(G,S):
     """
     pt_plus(G,S) returns the propagation time for
@@ -224,6 +354,7 @@ def full_pt_plus_int(G):
     if D == 0:
         return True
     return False
+"""
 
 def gzerosgame(g, F=None, B=None):
 	"""
